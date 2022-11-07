a3caa566661880b2aa43ee248d60697c77170a2a
MuradMagomedov <magomedoff84@gmail.com>
2022-11-07 11:33:28 +0300

    вынес футер в отдельный файл

--- kurtki.php
+++ kurtki.php
@@ -19,7 +19,7 @@
 
     <div class="container content ">
         <div class="product-item">
-            <img src="media/1.jpg">
+            <img src="media/k1.jpg">
             <div class="product-list">
                 <h3>Черный костюм(трикотаж)</h3>
                 <span class="price">₽ 1999</span>
@@ -27,7 +27,7 @@
             </div>
         </div>
         <div class="product-item">
-            <img src="media/2.jpg">
+            <img src="media/k2.jpg">
             <div class="product-list">
                 <h3>Черный костюм(трикотаж)</h3>
                 <span class="price">₽ 1999</span>
@@ -36,63 +36,19 @@
         </div>
 
         <div class="product-item">
-            <img src="media/3.jpg">
+            <img src="media/k3.jpg">
             <div class="product-list">
                 <h3>Зеленый костюм(трикотаж)</h3>
                 <span class="price">₽ 1999</span>
                 <a href="" class="button">В корзину</a>
             </div>
         </div>
-
-        <div class="product-item">
-            <img src="media/4.jpg">
-            <div class="product-list">
-                <h3>Серый костюм(трикотаж)</h3>
-                <span class="price">₽ 1999</span>
-                <a href="" class="button">В корзину</a>
-            </div>
-        </div>
-        <div class="product-item">
-            <img src="media/5.jpg">
-            <div class="product-list">
-                <h3>Серый костюм(трикотаж)</h3>
-                <span class="price">₽ 1999</span>
-                <a href="" class="button">В корзину</a>
-            </div>
-        </div>
-        <div class="product-item">
-            <img src="media/6.jpg">
-            <div class="product-list">
-                <h3>Серый костюм(трикотаж)</h3>
-                <span class="price">₽ 1999</span>
-                <a href="" class="button">В корзину</a>
-            </div>
-        </div>
-        <div class="product-item">
-            <img src="media/7.jpg">
-            <div class="product-list">
-                <h3>Серый костюм(трикотаж)</h3>
-                <span class="price">₽ 1999</span>
-                <a href="" class="button">В корзину</a>
-            </div>
-        </div>
-        <div class="product-item">
-            <img src="media/8.jpg">
-            <div class="product-list">
-                <h3>Серый костюм(трикотаж)</h3>
-                <span class="price">₽ 1999</span>
-                <a href="" class="button">В корзину</a>
-            </div>
-        </div>
-    </div>
+ </div>
     <footer class="top-menu" id="bottombar">
-        <ul class="menu-main">
-            <li> Мы работаем с лучшими поставщиками производителями одежды со всего
-                мира ~~~~~~~~~~~~~~~~~~~~~~ <a href="" class="current">НАВЕРХ</a>
-
-            </li>
-
-        </ul>
+    <?php
+        require_once('footer.php');   // подключаем footer                
+                                        // инфу взял отсюда https://ru.stackoverflow.com/questions/472675/%D0%94%D0%B8%D0%BD%D0%B0%D0%BC%D0%B8%D1%87%D0%B5%D1%81%D0%BA%D0%BE%D0%B5-%D0%BC%D0%B5%D0%BD%D1%8E-%D0%BD%D0%B0-php-%D0%BF%D0%BE%D0%B4%D1%81%D0%B2%D0%B5%D1%82%D0%BA%D0%B0-%D1%82%D0%B5%D0%BA%D1%83%D1%89%D0%B5%D0%B9-%D0%BA%D0%BD%D0%BE%D0%BF%D0%BA%D0%B8-%D0%B2-%D0%BC%D0%B5%D0%BD%D1%8E
+    ?>
     </footer>
 </body>
 <script src="js/script.js"></script>
