0459467712085adf92cade3b4c2990feb03fd9e0
MuradMagomedov <magomedoff84@gmail.com>
2022-11-13 19:28:43 +0300

    фича: добавил switch/case для соединения с БД через docker/openserver

--- db/dbconnect.php
+++ db/dbconnect.php
@@ -14,13 +14,38 @@
 //     print "Error!: " . $e->getMessage() . "<br/>";
 //     die();
 // }
-//Для ОпенСрвр
-$user='root';
-$pass='root';
-try {
 
-    $conn = new PDO('mysql:host=localhost;dbname=taiba', $user, $pass);
-} catch (PDOException $e) {
-    print "Error!: " . $e->getMessage() . "<br/>";
-    die();
-}
+
+    // if ($conn!==true) {
+    //                 $user='murad';
+    //                 $pass='murad';
+    //                 try {
+
+    //                     $conn = new PDO('mysql:host=db;dbname=murad', $user, $pass);
+    //                 } catch (PDOException $e) {
+    //                     print "Error!: " . $e->getMessage() . "<br/>";
+    //                     die();
+    //                 }
+    //                     } else {
+    //                 //Для ОпенСрвр
+    //                     $user='root';
+    //                     $pass='root';
+    //                     try {
+
+    //                         $conn = new PDO('mysql:host=localhost;dbname=taiba', $user, $pass);
+    //                     } catch (PDOException $e) {
+    //                         print "Error!: " . $e->getMessage() . "<br/>";
+    //                         die();
+    //                     }
+    //                     }
+        switch(true)
+        {
+        case $conn===true:
+            //Для  Докера
+            $conn = new PDO('mysql:host=db;dbname=murad', 'murad', 'murad');
+            break;
+        case
+            //Для Опенсервера
+            $conn = new PDO('mysql:host=localhost;dbname=taiba', 'root', 'root');
+            break;
+        }
