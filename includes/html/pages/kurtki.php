4438b9ac82e40b1ed458b74a3bebc97d58e314e3
MuradMagomedov <magomedoff84@gmail.com>
2022-12-01 00:34:21 +0300

    reature: сделал select where title куртка. refactor: разнес страницу вывода курток на MVC

--- includes/html/pages/kurtki.php
+++ includes/html/pages/kurtki.php
@@ -1,5 +1,5 @@
 <?php
-require_once("./includes/logic/cart/catalog.php")
+require_once("./includes/logic/pages/kurtki.php");
 ?>
 
 <div id="center">
---
+++ includes/logic/pages/kurtki.php
@@ -0,0 +1,11 @@
+<?php
+require_once("./includes/logic/cart/catalog.php");
+
+$query = "SELECT * FROM `products` WHERE `title` LIKE '%куртка%'";
+
+$req = mysqli_query($connection, $query);
+$data_from_db = [];
+
+while ($result = mysqli_fetch_assoc($req)) {
+    $data_from_db[] = $result;
+}
