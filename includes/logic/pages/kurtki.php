9cc710d83ee0719065635b5c23c9fc13633fcc40
MuradMagomedov <magomedoff84@gmail.com>
2022-12-02 23:12:58 +0300

    add: прописал сортировку селект из базы данных по селект форме

--- includes/logic/pages/kurtki.php
+++ includes/logic/pages/kurtki.php
@@ -1,22 +1,24 @@
 <?php
 require_once("./includes/logic/cart/db.php");
+include_once("./includes/html/formes/dropdown.php");
+
 
 
 $sql = "SELECT * FROM `products` WHERE `category_id` LIKE '2'";
 $select = $_POST['select'];
 switch ($select) {
     case "a_z":
-        $sql = "SELECT * FROM `products` WHERE `title` LIKE '%куртк%' ORDER BY `title` ";
+        $sql = "SELECT * FROM `products` WHERE `category_id` LIKE '2' ORDER BY `title` ";
         break;
     case "z_a":
-        $sql = "SELECT * FROM `products` WHERE `title` LIKE '%куртк%' ORDER BY `title` DESC";
+        $sql = "SELECT * FROM `products` WHERE `category_id` LIKE '2' ORDER BY `title` DESC";
         break;
     case "priceMax":
-        $sql = "SELECT * FROM `products` WHERE `title` LIKE '%куртк%' ORDER BY `price`";
+        $sql = "SELECT * FROM `products` WHERE `category_id` LIKE '2' ORDER BY `price` DESC";
         break;
 
     case "priceMin":
-        $sql = "SELECT * FROM `products` WHERE `title` LIKE '%куртк%' ORDER BY `price` DESC ";
+        $sql = "SELECT * FROM `products` WHERE `category_id` LIKE '2' ORDER BY `price`  ";
         break;
 }
 $req = mysqli_query($connection, $sql);
@@ -25,16 +27,3 @@ $data_from_db = [];
 while ($result = mysqli_fetch_assoc($req)) {
     $data_from_db[] = $result;
 }
-
-
-?>
-
-<form action="" method="post">
-    <select name='select'>
-        <option value='a_z' selected>А-Б</option>
-        <option value='z_a'>Б-А</option>
-        <option value='priceMax'>по цене (дороже)</option>
-        <option value='priceMin'>по цене (дешевле)</option>
-    </select>
-    <input class="dropbtn" type='submit' name='submit' value='отсортировать'>
-</form>
--- includes/logic/pages/trikotaj.php
+++ includes/logic/pages/trikotaj.php
@@ -1,9 +1,26 @@
 <?php
-require_once("./includes/logic/cart/catalog.php");
+require_once("./includes/logic/cart/db.php");
+include_once("./includes/html/formes/dropdown.php");
 
-$query = "SELECT * FROM `products` WHERE `title` LIKE '%трикота%'";
 
-$req = mysqli_query($connection, $query);
+$sql = "SELECT * FROM `products` WHERE `category_id` LIKE '3'";
+$select = $_POST['select'];
+switch ($select) {
+    case "a_z":
+        $sql = "SELECT * FROM `products` WHERE `category_id` LIKE '3' ORDER BY `title` ";
+        break;
+    case "z_a":
+        $sql = "SELECT * FROM `products` WHERE `category_id` LIKE '3' ORDER BY `title` DESC";
+        break;
+    case "priceMax":
+        $sql = "SELECT * FROM `products` WHERE `category_id` LIKE '3' ORDER BY `price` DESC";
+        break;
+
+    case "priceMin":
+        $sql = "SELECT * FROM `products` WHERE `category_id` LIKE '3' ORDER BY `price`  ";
+        break;
+}
+$req = mysqli_query($connection, $sql);
 $data_from_db = [];
 
 while ($result = mysqli_fetch_assoc($req)) {
