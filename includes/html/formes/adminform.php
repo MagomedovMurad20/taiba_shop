ead6d3c108fceb4b8fa24d4298c44032f69cca2e
MuradMagomedov <magomedoff84@gmail.com>
2022-12-06 20:46:42 +0300

    #css/html: привел все формы в единый стиль

--- includes/html/formes/adminform.php
+++ includes/html/formes/adminform.php
@@ -1,30 +1,53 @@
        <?php
         ?>
 
-       <h3>Загрузка товара</h3>
-       <form method="post" enctype="multipart/form-data" id="adminform">
-           <p>Название <br />
-               <input type="text" name="title" min="3" autofocus required" placeholder="Название товара" />
-           </p>
-           <p>Описание <br />
-               <input type="text" name="description" min="50" max="500" autofocus required placeholder="Описание товара" />
-           </p>
-           <p>Цена <br />
-               <input type="number" name="price" id="price" required /> руб.
-           </p>
-           <p>Фото <br />
-               <input type="file" name="img" id="img" accept=".jpg, .jpeg, .png" required />
-           </p>
-           <input type="submit" value="Загрузить товар">
-           <select name="category_id">
+       <div class="ufive-admin">
+           <form method="post" enctype="multipart/form-data" id="adminform">
+               <section id="feedback">
 
+                   <h3>Загрузка товара</h3>
+                   <br />
+                   <div class="field">
+                       <label for="title">Цена товара</label>
 
-               <?php
-                require_once './includes/logic/cart/db.php';
-                require_once './includes/logic/formes/adminformselect.php';
-                ?>
-           </select>
-       </form>
+                       <input type="text" name="title" min="3" autofocus required" placeholder="Название товара" />
+                   </div>
+                   <div class="field">
+                       <label for="description">Описание товара
+
+                       </label>
+
+                       <input type="text" name="description" min="50" max="500" autofocus required placeholder="Описание товара" />
+                   </div>
+
+                   <div class="field">
+                       <label for="number">Цена товара</label>
+
+                       <input type="number" name="price" id="price" required /> руб.
+                   </div>
+                   <br />
+                   <div class="field">
+                       <label for="img">Фото</label>
+
+                       <input type="file" name="img" id="img" accept=".jpg, .jpeg, .png" required />
+                   </div>
+                   </p>
+
+                   <div class="field">
+                       <label for="category_id">Категория</label>
+                       <select name="category_id">
+                           <?php
+                            require_once './includes/logic/cart/db.php';
+                            require_once './includes/logic/formes/adminformselect.php';
+                            ?>
+                       </select>
+                   </div>
+                   <label for="name"></label>
+
+                   <input type="submit" value="Загрузить товар">
+               </section>
+           </form>
+       </div>
        <script>
            document.getElementById("adminform").reset();
        </script>
--- includes/logic/formes/adminformselect.php
+++ includes/logic/formes/adminformselect.php
@@ -1,11 +1,11 @@
 <?php
 $query = "SELECT * FROM `categories`";
-                $req = mysqli_query($connection, $query);
-                $categories = [];
-                while ($result = mysqli_fetch_assoc($req)) {
-                $categories[] = $result;
-                }
-                foreach ($categories as $category) {
+$req = mysqli_query($connection, $query);
+$categories = [];
+while ($result = mysqli_fetch_assoc($req)) {
+    $categories[] = $result;
+}
+foreach ($categories as $category) {
 
-                echo "<option value=\"{$category['id']}\">{$category['title']}</option>";
-                }
+    echo "<option value=\"{$category['id']}\">{$category['title']}</option>";
+}
