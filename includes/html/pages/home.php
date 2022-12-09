20642fc60499757144e3ff6d19212b8c8aebc99b
MuradMagomedov <magomedoff84@gmail.com>
2022-12-09 15:09:36 +0300

    #php optimize: оптимизировал отработку формы

--- includes/html/pages/home.php
+++ includes/html/pages/home.php
@@ -1,5 +1,5 @@
 <?php
 require_once("./includes/html/header.php");
-require_once("./includes/html/templates/product_item.php");
 require_once("./includes/logic/cart/catalog.php");
+require_once("./includes/html/templates/product_item.php");
 require_once("./includes/html/footer.php");
--- includes/logic/formes/adminform.php
+++ includes/logic/formes/adminform.php
@@ -1,58 +1,60 @@
 <?php
-
-if ($_POST['email'] == '' || $_POST['password'] == '') {
-    require_once("./includes/html/formes/adminform.php");
-}
-
-try {
-    require_once './db/dbconnect.php';
+require_once("./includes/html/formes/adminform.php");
 
 
-    $sql = "INSERT INTO `products` (title, description, price, img, category_id) VALUES (?, ?, ?, ?, ?)";
+if ($_POST['email'] !== '' || $_POST['password'] !== '') {
 
-    if (is_uploaded_file($_FILES['img']['tmp_name'])) {
-        $img = file_get_contents($_FILES['img']['tmp_name']);
-    }
-    // определяем prepared statement
-    $stmt = $conn->prepare($sql);
-    // привязываем параметры к значениям
-    $rowsNumber = $stmt->execute(array(
-        $_POST["title"],
-        $_POST["description"],
-        $_POST["price"],
-        $img,
-        $_POST['category_id']
-    ));
+    try {
+        $sql = "INSERT INTO `products` (title, description, price, img, category_id) VALUES (?, ?, ?, ?, ?)";
+
+        if (is_uploaded_file($_FILES['img']['tmp_name'])) {
+            $img = file_get_contents($_FILES['img']['tmp_name']);
+        }
+        // определяем prepared statement
+        $stmt = $conn->prepare($sql);
+        // привязываем параметры к значениям
+        $rowsNumber = $stmt->execute(array(
+            $_POST["title"],
+            $_POST["description"],
+            $_POST["price"],
+            $img,
+            $_POST['category_id']
+        ));
 
 
-    // если добавлена как минимум одна строка
-    if ($rowsNumber > 0) {
-        echo "товар успешно добавлен: 
+        // если добавлена как минимум одна строка
+        if ($rowsNumber > 0) {
+            echo "товар успешно добавлен: 
         название=" . $_POST["title"]
-            . "  описание= "  . $_POST["description"]
-            . "цена=" . $_POST["price"]
-            . "фото=" . $_POST["img"];
+                . "  описание= "  . $_POST["description"]
+                . "цена=" . $_POST["price"]
+                . "фото=" . $_POST["img"];
+        } else {
+            echo "ko";
+        }
+    } catch (PDOException $e) {
+        echo "Database error: " . $e->getMessage();
     }
-} catch (PDOException $e) {
-    echo "Database error: " . $e->getMessage();
+
+
+
+    //todo закрывать соединение скл
+
+    // $querycheck = 'SELECT * from `products`';
+    // $query_result = $conn->query($querycheck);
+
+    // if ($query_result !== FALSE) {
+    // echo "table exist";
+    // } else {
+    // $conn->query('CREATE TABLE products (
+    // id int(11) AUTO_INCREMENT,
+    // title varchar(50) NOT NULL,
+    // description varchar(300) NOT NULL,
+    // price varchar(10) NOT NULL,
+    // img longblob NOT NULL,
+    // PRIMARY KEY(id)
+    // )');
+    // }
+} else {
+    echo "cho";
 }
-
-
-
-//todo закрывать соединение скл
-
-// $querycheck = 'SELECT * from `products`';
-// $query_result = $conn->query($querycheck);
-
-// if ($query_result !== FALSE) {
-// echo "table exist";
-// } else {
-// $conn->query('CREATE TABLE products (
-// id int(11) AUTO_INCREMENT,
-// title varchar(50) NOT NULL,
-// description varchar(300) NOT NULL,
-// price varchar(10) NOT NULL,
-// img longblob NOT NULL,
-// PRIMARY KEY(id)
-// )');
-// }
