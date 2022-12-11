046741d338d24a11f0e95cfcc6b8fbac395d0ace
MuradMagomedov <magomedoff84@gmail.com>
2022-12-11 23:32:44 +0300

    #php/sql optimize: оптимизировал sql запросы

--- includes/logic/formes/form_select_product.php
+++ includes/logic/formes/form_select_product.php
@@ -3,48 +3,49 @@
 require_once($_SERVER['DOCUMENT_ROOT'] . "/includes/logic/cart/db.php");
 
 $category_id = $_GET['category_id'];
-$sql = "SELECT * FROM `products` WHERE `category_id`  $category_id";
+$sql = "SELECT * FROM `products` WHERE `category_id` = $category_id";
 $select = $_POST['select'];
 switch ($select) {
 
     case "":
-        $sql = "SELECT * FROM `products` WHERE `category_id` LIKE $category_id ";
+        $sql;
         break;
     case "a_z":
-        $sql = "SELECT * FROM `products` WHERE `category_id` LIKE $category_id ORDER BY `title` ";
+        $sql .= " ORDER BY `title` ";
         break;
     case "z_a":
-        $sql = "SELECT * FROM `products` WHERE `category_id` LIKE $category_id ORDER BY `title` DESC";
+        $sql .= "ORDER BY `title` DESC";
         break;
     case "priceMax":
-        $sql = "SELECT * FROM `products` WHERE `category_id` LIKE $category_id ORDER BY `price` DESC";
+        $sql .= "ORDER BY `price` DESC";
         break;
 
     case "priceMin":
-        $sql = "SELECT * FROM `products` WHERE `category_id` LIKE $category_id ORDER BY `price`  ";
+        $sql .= "ORDER BY `price`  ";
         break;
 }
 
 
 
 if (!$category_id) {
+    $sql = "SELECT * FROM `products` ";
     switch ($select) {
 
         case "":
-            $sql = "SELECT * FROM `products` ";
+            $sql;
             break;
         case "a_z":
-            $sql = "SELECT * FROM `products` ORDER BY `title` ";
+            $sql .= "ORDER BY `title` ";
             break;
         case "z_a":
-            $sql = "SELECT * FROM `products`  ORDER BY `title` DESC";
+            $sql .= "ORDER BY `title` DESC";
             break;
         case "priceMax":
-            $sql = "SELECT * FROM `products` WHERE `category_id`  ORDER BY `price` DESC";
+            $sql .= " ORDER BY `price` DESC";
             break;
 
         case "priceMin":
-            $sql = "SELECT * FROM `products` WHERE `category_id`  ORDER BY `price`  ";
+            $sql .= " ORDER BY `price`  ";
             break;
     }
 }
