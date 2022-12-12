608da0e6119f886629d6388f1714e293d584b617
MuradMagomedov <magomedoff84@gmail.com>
2022-12-12 18:39:58 +0300

    #php/sql fix: добавил соли к хешам паролей

--- includes/logic/formes/signupform.php
+++ includes/logic/formes/signupform.php
@@ -1,7 +1,17 @@
 <?php
 
-//require_once '../../../db/dbconnect.php';
+//генерируем соль;
+function generateSalt()
+{
+    $salt = '';
+    $saltLength = 8; // длина соли
 
+    for ($i = 0; $i < $saltLength; $i++) {
+        $salt .= chr(mt_rand(33, 126)); // символ из ASCII-table
+    }
+
+    return $salt;
+};
 
 if ($_POST['email'] == '' || $_POST['password'] == '') {
     require_once($_SERVER['DOCUMENT_ROOT'] . "/includes/html/formes/signupform.php");
@@ -15,7 +25,8 @@ if ($_POST['email'] == '' || $_POST['password'] == '') {
         // определяем prepared statement
         $stmt = $conn->prepare($sql);
         // привязываем параметры к значениям
-        $rowsNumber = $stmt->execute(array($_POST["email"], md5($_POST["password"])));
+        $salt = generateSalt(); //добавляем соли  в пароль
+        $rowsNumber = $stmt->execute(array($_POST["email"], md5($salt . $_POST["password"])));
 
         // если добавлена как минимум одна строка
         if ($rowsNumber > 0) {
