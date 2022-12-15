3297f39bca39bd41431fa88e1d885f604842b100
MuradMagomedov <magomedoff84@gmail.com>
2022-12-15 11:29:10 +0300

    #php/css feat: добавил картинку ссылку для корзины, добавил стилей

--- includes/logic/cart/small_cart.php
+++ includes/logic/cart/small_cart.php
@@ -2,9 +2,14 @@
 require_once($_SERVER['DOCUMENT_ROOT'] . "/db/dbconnect.php");
 
 session_start();
-require_once($_SERVER['DOCUMENT_ROOT'] . "/includes/html/templates/small_cart.php");
 
-if (isset($_SESSION['cart_list'])) {
-	echo "Корзина: " . count($_SESSION['cart_list']) . " товара";
-	echo " ";
-}
+if (isset($_SESSION['cart_list'])) { ?>
+
+	<div class="small-cart">
+
+		<a href="#" onclick="cartOpen();">
+			<img src="/media/cart.jpg">
+			Корзина: <?php echo count($_SESSION['cart_list']) ?> товара
+		</a>
+	</div>
+<?php } ?>
